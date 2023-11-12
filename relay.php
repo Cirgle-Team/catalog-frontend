38480fee8f1eec8b74150bb1d8b601b62bff0543
hybocosine <donghwi04@naver.com>
2023-11-12 11:55:42 +0900

    beautiful harmonization with Front and Back

---
+++ relay.php
@@ -0,0 +1,36 @@
+<?php
+error_reporting( E_ALL );
+ini_set( "display_errors", 1 );
+if (!isset($_POST["req"], $_POST["path"])) {
+    die("{\"code\":\"invalid-000\", \"message\":\"Invalid Request\"}");
+}
+
+$BASE_URL = "http://localhost:8080/";
+$headers = array('Content-Type: application/json');
+if (isset($_POST["header"])) {
+    array_push($headers, $_POST["header"]);
+}
+
+$ch = curl_init($BASE_URL.$_POST["path"]);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_POST, true);
+if (isset($_POST["method"])) {
+    if ($_POST["method"] == "GET") {
+        curl_setopt($ch,CURLOPT_POST, false);
+    }
+} else {
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $_POST["req"]);
+}
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($ch);
+if (strpos($result, "GMT") !== FALSE) {
+    if (strpos($result, "close") !== FALSE) {
+        die(substr($result, strpos($result, "close")+7));
+    } else {
+        die(substr($result, strpos($result, "GMT")+4));
+    }
+} else {
+    die(substr($result, strpos($result, "close")+7));
+}
+?>
